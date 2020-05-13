dc5af7d751aa1b50eebece2c2d93d0bdbfd5f937
i47uen <jeealay8@gmail.com>
2020-05-13 07:48:54 +0700

    update

---
+++ src/blocks/modules/footer/footer.js
@@ -0,0 +1,14 @@
+modules.define('footer', ['i-bem-dom'], function(provide, bemDom) {
+
+    provide(bemDom.declBlock(this.name, {
+        onSetMod: {
+            js: {
+                inited: function() {
+                    
+                }
+            }
+        }
+    }));
+    
+    });
+    
